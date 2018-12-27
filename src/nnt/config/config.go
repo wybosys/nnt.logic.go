f8e96137783ef2c665881d2756ffc11d3390dcfb
wybosys <wybosys@gmail.com>
2018-12-27 21:03:55 +0800

    prj

--- src/nnt/config/config.go
+++ src/nnt/config/config.go
@@ -1,2 +1,56 @@
 package config
 
+import (
+	"nnt/core/kernel"
+	"nnt/core/stl"
+	"nnt/manager"
+	"strings"
+)
+
+func NodeIsEnable(cfg *kernel.JsonObject) bool {
+	v, ok := cfg.CheckGet("enable")
+	if !ok {
+		return true;
+	}
+
+	enable, _ := v.String()
+	conds := stl.StringArray(strings.Split(enable, ","))
+	_, fnd := conds.Query(func(idx int, e string) bool {
+		if e == "" {
+			return false
+		}
+		if e == "debug" {
+			return manager.DEBUG
+		}
+		if e == "develop" {
+			return manager.DEVELOP
+		}
+		if e == "publish" {
+			return manager.PUBLISH
+		}
+		if e == "distribution" {
+			return manager.DISTRIBUTION
+		}
+		if e == "release" {
+			return manager.PUBLISH || manager.DISTRIBUTION
+		}
+		if e == "devops" {
+			return manager.DEVOPS
+		}
+		if e == "devops-develop" {
+			return manager.DEVOPS_DEVELOP
+		}
+		if e == "devops-release" {
+			return manager.DEVOPS_RELEASE
+		}
+		if e == "local" {
+			return manager.LOCAL
+		}
+		if kernel.ArgsContains("--" + e) {
+			return true
+		}
+		return false
+	})
+
+	return fnd
+}
--- src/nnt/config/node.go
+++ src/nnt/config/node.go
@@ -1,55 +1,12 @@
 package config
 
-import (
-	"nnt/core/kernel"
-	"nnt/core/stl"
-	"strings"
-)
+type Node struct {
+	// 节点id
+	Id string
 
-func NodeIsEnable(cfg *kernel.JsonObject) bool {
-	v, ok := cfg.CheckGet("enable")
-	if !ok {
-		return true;
-	}
+	// 实体对应的类
+	Entry string
 
-	enable, _ := v.String()
-	conds := stl.StringArray(strings.Split(enable, ","))
-	_, fnd := conds.Query(func(idx int, e string) bool {
-		if e == "" {
-			return false
-		}
-		if e == "debug" {
-			return DEBUG
-		}
-		if e == "develop" {
-			return DEVELOP
-		}
-		if e == "publish" {
-			return PUBLISH
-		}
-		if e == "distribution" {
-			return DISTRIBUTION
-		}
-		if e == "release" {
-			return PUBLISH || DISTRIBUTION
-		}
-		if e == "devops" {
-			return DEVOPS
-		}
-		if e == "devops-develop" {
-			return DEVOPS_DEVELOP
-		}
-		if e == "devops-release" {
-			return DEVOPS_RELEASE
-		}
-		if e == "local" {
-			return LOCAL
-		}
-		if kernel.ArgsContains("--" + e) {
-			return true
-		}
-		return false
-	})
-
-	return fnd
+	// 开发模式配置, 如果不配置，则代表任何模式都启用，否则只有命中的模式才启用
+	Enable string
 }
